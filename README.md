83c0b86ee9dc62de64330cd846829fb900819023
Shlok Jhawar <shlokj@gmail.com>
2023-04-27 18:55:20 -0700

    assignment complete - squashed all commits and cleaned up

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
-## UID: 123456789
-(IMPORTANT: Only replace the above numbers with your true UID, do not modify spacing and newlines, otherwise your tarfile might not be created correctly)
+## UID: XXXXXXXXX
 
 ## Pipe Up
 
-One sentence description
+This program replicates the functionality of the pipe operator (|). It pipes the output of the first program into the second, the output of the second into the third, and so on until the last one.
 
 ## Building
 
-Explain briefly how to build your program
+To build the executable, simply run `make` in the folder where the Makefile and all other files are present.
 
 ## Running
 
-Show an example run of your program, using at least two additional arguments, and what to expect
+This program produces output identical to that of the the pipe operator. To run this program, run `./pipe` followed by all the arguments (Linux commands you want to pipe).
+
+Example 1: `./pipe ls cat wc`
+
+<img width="537" alt="image" src="https://user-images.githubusercontent.com/34567765/235278919-5a5fb37c-08af-4751-adab-3157fa3e9765.png">
+
+This checks out, because there are 8 lines printed, each with one filename, totaling 75 characters. The `cat` simply propagates the output forward in this case.
+
+Example 2: `./pipe cal echo pwd`
+
+<img width="577" alt="image" src="https://user-images.githubusercontent.com/34567765/235279447-070006fa-44e4-4457-9042-d25aba03f72f.png">
+
+The `cal` output, which is `echo`'d is ignored by `pwd` since its job is to print the current working directory.
+
+Similarly, you can pipe any (reasonable) number of arguments together.
 
 ## Cleaning up
 
-Explain briefly how to clean up all binary files
+To clean up all binary files, run `make clean`.
--- pipe.c
+++ pipe.c
@@ -1,4 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+#define EXIT_FAILURE 1
+
 int main(int argc, char *argv[])
 {
-	return 0;
+	errno = 0;	
+	if (argc <= 1) {
+		errno = EINVAL;
+		exit(errno);
+	}
+
+	int fds[2];
+
+	if (pipe(fds)) {
+		exit(EXIT_FAILURE);
+	}
+
+	int rc = fork();
+	if (rc < 0) {
+		exit(EXIT_FAILURE);
+	}
+
+	else if (rc == 0) {
+		if (argc > 2) {
+			dup2(fds[1], STDOUT_FILENO);
+		}
+		if (execlp(argv[1], argv[1], (char *) NULL) == -1) {
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	else {
+		int status = 0;
+		waitpid(rc, &status, 0);
+		close(fds[1]);
+		errno = WEXITSTATUS(status);
+		if (errno != 0) 
+			exit(errno);
+	}
+
+	for (int i = 2; i < argc - 1; i++) {
+		int fd_read = fds[0];
+		
+		if (pipe(fds)) {
+			exit(EXIT_FAILURE);
+		}	
+
+		int rc1 = fork();
+		if (rc1 < 0) {
+			exit(EXIT_FAILURE);
+		}
+
+		else if (rc1 == 0) {
+			dup2(fds[1], STDOUT_FILENO);
+			dup2(fd_read, STDIN_FILENO);
+			if (execlp(argv[i], argv[i], (char *) NULL) == -1) {
+				exit(EXIT_FAILURE);
+			}
+		}
+		else {
+			int status = 0;
+			waitpid(rc1, &status, 0);
+			close(fds[1]);
+			close(fd_read);
+			errno = WEXITSTATUS(status);
+			if (errno != 0) 
+				exit(errno);
+		}
+	}
+
+	if (argc >= 3) {
+		int rc_last = fork();
+		if (rc_last < 0) {
+			exit(EXIT_FAILURE);
+		}
+
+		else if (rc_last == 0) {
+			dup2(fds[0], STDIN_FILENO);
+			if (execlp(argv[argc-1], argv[argc-1], (char *) NULL) == -1) {
+				exit(EXIT_FAILURE);
+			}		
+		}
+
+		else {
+			int status = 0;
+			waitpid(rc_last, &status, 0);
+			close(fds[1]);
+			errno = WEXITSTATUS(status);
+			if (errno != 0) 
+				exit(errno);
+		}
+	}
+	return errno;
 }
